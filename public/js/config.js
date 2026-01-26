632f0c38a48e0cdd3ab9222170d5559bf00c3e3d
Anukrish-sap <240134909@aston.ac.uk>
2026-01-26 20:45:20 +0000

    linking the database

---
+++ public/js/config.js
@@ -0,0 +1,7 @@
+// public/js/config.js
+// Publishable (anon) key only. Do NOT put service_role in frontend.
+
+window.QUIZZAS_CONFIG = {
+  SUPABASE_URL: "https://ywqkgttthlpytpwaxwpr.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_nrvv6YM3tKg0NZL1Bkvk0w_tlVWRoEA",
+};
